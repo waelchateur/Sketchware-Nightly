5219786c8a025888cc4293115206489c515c6587
aikr <farzasz7@gmail.com>
2025-08-22 20:48:15 +0700

    fix: Block embedded code textfield was single line.

--- app/src/main/java/mod/hilal/saif/activities/tools/BlocksManagerCreatorActivity.java
+++ app/src/main/java/mod/hilal/saif/activities/tools/BlocksManagerCreatorActivity.java
@@ -302,7 +302,6 @@ public class BlocksManagerCreatorActivity extends BaseAppCompatActivity {
         binding.spec2.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
         binding.spec2.setMaxLines(1);
         binding.code.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-        binding.code.setMinLines(10);
         binding.spec2Layout.setVisibility(View.GONE);
     }
 
